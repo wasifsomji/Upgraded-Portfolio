d029afbf2c976c84fa3a6b1a6844ed148ebbfccc
wasifsomji <wasifsomji@gmail.com>
2025-04-13 13:49:23 -0400

    update LS experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -148,7 +148,9 @@ import {
       iconBg: "#000000",
       date: "Jan 2025 - Apr 2025",
       points: [
-        "Microservices Development @ X-Series: working with Go, GraphQL, React, and TypeScript"
+        "Implemented inventory adjustment reports and custom adjustment reasons using a GraphQL, Protobuf, and gRPC architecture in Go with a responsive React frontend and MySQL database, increasing reporting views by 27% and contributing to 3+ new client acquisitions post-release.",
+        "Designed and implemented 150+ unit and integration tests across 8+ Go microservices, significantly improving test coverage and reducing regression bugs in production releases.",
+        "Utilized React + TypeScript to contribute to the integration of Lightspeed X-Series’s employee management system with Homebase for seamless employee clock-in/out functionality, enabling support for 100+ retailers."
       ],
     },
     {
