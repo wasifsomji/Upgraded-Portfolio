08cb62bf06a3746e20eff7d1463b28529480770e
wasifsomji <wasifsomji@gmail.com>
2025-04-13 13:50:01 -0400

    update ls

--- src/constants/index.js
+++ src/constants/index.js
@@ -149,7 +149,7 @@ import {
       date: "Jan 2025 - Apr 2025",
       points: [
         "Implemented inventory adjustment reports and custom adjustment reasons using a GraphQL, Protobuf, and gRPC architecture in Go with a responsive React frontend and MySQL database, increasing reporting views by 27% and contributing to 3+ new client acquisitions post-release.",
-        "Designed and implemented 150+ unit and integration tests across 8+ Go microservices, significantly improving test coverage and reducing regression bugs in production releases.",
+        "Designed and deployed 120+ unit and integration tests across 8+ Go microservices, significantly improving test coverage and reducing regression bugs in production releases.",
         "Utilized React + TypeScript to contribute to the integration of Lightspeed X-Series’s employee management system with Homebase for seamless employee clock-in/out functionality, enabling support for 100+ retailers."
       ],
     },
